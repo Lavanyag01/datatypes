8e195cb3be5b59e90e245dfa3aac5d4aa1b17393
Lavanyag01 <72540566+Lavanyag01@users.noreply.github.com>
2020-10-20 15:04:54 +0530

    Add files via upload

---
+++ 04datatype/DataTypeDemo2.java
@@ -0,0 +1,44 @@
+class DataTypeDemo2
+{
+	public static void main(String[] args) 
+	{
+		//System.out.println("Hello World!");
+		/*byte b=10;
+	System.out.println("the value of b is " +b);
+	b=130;
+	System.out.println("the value of b is " +b);
+	b=(byte)130;
+	System.out.println("the value of b is " +b);*/
+	//short s=23678;
+		//System.out.println("the value of s is " +s);
+
+	//short s=45000;
+	//short s=(short)45678;
+	
+		//System.out.println("the value of s is " +s);
+
+		//int i=62678;
+		//System.out.println("the value of i is " +i);
+
+		//int i=(int)6737423455;
+		//System.out.println("the value of i is " +i);
+
+		//float f=123;
+		//System.out.println("the value of f is " +f);
+      // float f=123.5f;
+		//float 2f=13.7;
+
+		//System.out.println("the value of f is " +f);
+
+
+// double precision with standard notation
+
+		//double d= 4567.88867;
+		//System.out.println("the value of d is " +d);
+
+		//double precision with scientific notation
+
+		double d= 4567e-4;
+		System.out.println("the value of d is " +d);
+	}
+}
